143ba135a48132550008696f37e0289f116db891
jiyun <ij447504@gmail.com>
2026-05-26 15:21:38 +0900

    bible-read-api 2

--- qtai-server/src/main/java/com/qtai/domain/bible/internal/BibleService.java
+++ qtai-server/src/main/java/com/qtai/domain/bible/internal/BibleService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+//이지윤윤
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
